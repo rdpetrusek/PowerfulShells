acc763ae8e0614363d840376a43934637a5c5b57
rdpetrusek <dpetrusek@dwhomes.com>
2018-01-13 20:47:24 -0600

    Create MSMQ_Queue_write_transactional.ps1

---
+++ MSMQ_Queue_write_transactional.ps1
@@ -0,0 +1,22 @@
+#example WriteMessageToMSMQTrans "FormatName:DIRECT=OS:{_server_name_}\private$\{_queue_name_}" "<messagebody></messagebody>" "message label!"
+
+
+function WriteMessageToMSMQTrans($queueName, $message, $label)
+  {
+     $queue = new-object System.Messaging.MessageQueue $queueName
+     $utf8 = new-object System.Text.UTF8Encoding
+     $tran = new-object System.Messaging.MessageQueueTransaction
+     $tran.Begin()
+     $msgBytes = $utf8.GetBytes($message)
+     $msgStream = new-object System.IO.MemoryStream
+     $msgStream.Write($msgBytes, 0, $msgBytes.Length)
+     $msg = new-object System.Messaging.Message
+     $msg.BodyStream = $msgStream   
+     if ($label -ne $null)
+       {
+         $msg.Label = $label
+       }
+     $queue.Send($msg, $tran)
+     $tran.Commit()
+     Write-Host "Message written: " $message
+  }
